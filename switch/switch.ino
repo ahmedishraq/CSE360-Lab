7e571e6c450329941a021ce1d4f96d38ef1df8f6
ISHRAQ AHMED ESHA <19301261@bracu.ac.bd>
2022-06-27 15:16:34 +0600

    led buzzer with switch control

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,16 +1,23 @@
 int ledpin = 7;
 int button = 8;
+int buzzer = 6;
 
 void setup() {
 pinMode(ledpin, OUTPUT);
-pinMode(button, INPUT);
-
+pinMode(button, INPUT_PULLUP); // INPUT_PULLUP act as in build resistor for button
+pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
-digitalWrite(ledpin,HIGH);
-delay(1000);
-digitalWrite(ledpin,LOW);
-delay(1000);
+  if(digitalRead(button) == LOW){
+      digitalWrite(ledpin, HIGH);
+      digitalWrite(buzzer, HIGH);
+      delay(1000);    
+  }
+  else{
+      digitalWrite(ledpin, LOW);
+      digitalWrite(buzzer, LOW);
+      delay(1000);
+  }
 
 }
